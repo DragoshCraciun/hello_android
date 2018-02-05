8c1ef374bbef1d77927ec04d57d2fa47c7a12df1
dragosh craciun <dragosh.craciun@gmail.com>
2018-02-05 21:29:43 +0100

    feat: Intents for phone, web and map

--- app/src/main/java/com/galacticcoders/helloandroid/MainActivity.java
+++ app/src/main/java/com/galacticcoders/helloandroid/MainActivity.java
@@ -1,7 +1,10 @@
 package com.galacticcoders.helloandroid;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dialPhoneNumber(View view) {
+        Intent makeCall = new Intent(Intent.ACTION_DIAL);
+        makeCall.setData(Uri.parse("tel:6505555555" ));
+        if (makeCall.resolveActivity(getPackageManager()) != null) {
+            startActivity(makeCall);
+        }
+    }
+
+    public void openWebPage(View view) {
+        Uri webpage = Uri.parse("https://eu.udacity.com/");
+        Intent openBrowser = new Intent(Intent.ACTION_VIEW, webpage);
+        if (openBrowser.resolveActivity(getPackageManager()) != null) {
+            startActivity(openBrowser);
+        }
+    }
+
+    public void showMap(View view) {
+        Intent openMap = new Intent(Intent.ACTION_VIEW);
+        openMap.setData(Uri.parse("geo:37.3999001, -122.1105892"));
+        if (openMap.resolveActivity(getPackageManager()) != null) {
+            startActivity(openMap);
+        }
     }
 
 }
